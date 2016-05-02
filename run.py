93eefa68c8bf71eceff54a7c14a949fb1d4e1699
shirt <kirk.gardner@uconn.edu>
2016-05-01 21:47:55 -0400

    transcript

--- run.py
+++ run.py
@@ -68,9 +68,9 @@ class Mapper:
 		i = 0
 		for v in self.complex.vertices:
 			for sample in v.cluster.samples:
-				print len(sample.clusters)
+				# print len(sample.clusters)
 				if len(sample.clusters) == 1:
-					print "class"+str(i)+"\t"+sample.label[:12]
+					# print "class"+str(i)+"\t"+sample.label[:12]
 					fle.write("class"+str(i)+"\t"+sample.label[:12]+"\n")
 				# transcript[i].append(sample.label)
 			i = i + 1
